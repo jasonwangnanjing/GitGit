64155bf19d4720d31dc8a5ff35021d53a89b437b
jason <jinqu7777@163.com>
2017-11-08 23:06:38 +0800

    update height's method

--- springmvc/src/MainController.java
+++ springmvc/src/MainController.java
@@ -19,6 +19,14 @@ public class MainController {
     private String name;
  private int age;
 
-private double height;
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    private double height;
 
 }
