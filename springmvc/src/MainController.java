c790cfdcf3e88209a98d5461da9059a0551d6012
jason <jinqu7777@163.com>
2017-11-08 23:20:05 +0800

    Merge branch 'master' of https://github.com/jasonwangnanjing/GitGit
    # Conflicts:
    #	springmvc/src/MainController.java

--- springmvc/src/MainController.java
+++ springmvc/src/MainController.java
@@ -19,8 +19,26 @@ public class MainController {
     private String name;
  private int age;
 
-private double height;
+
+
+    private double height;
     
     private double weight;
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 
 }
