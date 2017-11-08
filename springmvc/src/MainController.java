d51b440857569ba1cafdd366d17f58be012fe95d
jason <jinqu7777@163.com>
2017-11-08 22:00:19 +0800

    firs commit from local

---
+++ springmvc/src/MainController.java
@@ -0,0 +1,23 @@
+
+public class MainController {
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private String name;
+ private int age;
+
+
+}
